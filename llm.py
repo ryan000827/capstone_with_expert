349e61c663cdaa96b364df1a8e33d340d22687c7
Ryan Ye <ryan_ye@u.yale-nus.edu.sg>
2025-02-10 12:22:47 +0800

    commit

--- llm.py
+++ llm.py
@@ -11,7 +11,7 @@ openai_model = st.secrets['OPENAI_MODEL']
 # Create the LLM
 llm = ChatOpenAI(
     openai_api_key=st.secrets["OPENAI_API_KEY"],
-    model=st.secrets["OPENAI_MODEL"],
+    model=st.secrets["OPENAI_MODEL"]
 )
 
 # Create the Embedding model
